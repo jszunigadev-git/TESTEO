9524ff0723dbe0f6f9f15650c9ab65b8f73edb1b
Jose Zuñiga <jszuniga.dev@gmail.com>
2026-06-23 13:09:12 -0400

    refactor(sumando.py): encaptular logica de la suma en una función

--- sumando.py
+++ sumando.py
@@ -1,5 +1,14 @@
-num_1: int = 33
-num_2: int = 33
 
-suma = num_1 + num_2
-print(suma)
+def sumando(*args:int|float):
+    if not all(isinstance(num,(int,float)) for num in args ):
+        raise TypeError("Error, Debe ingresar valores numericos")
+    return  sum(args)
+
+num_1: int = 33
+num_2: int = 339
+
+try:
+    resultado = sumando(num_1,num_2)
+    print(resultado)
+except TypeError as e:
+    print(f"Se detectó un problema: {e}")
